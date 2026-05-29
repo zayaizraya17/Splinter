b5a269a12e9834e6e3654aecd983c43740773344
kira <dolgaakira@gmail.com>
2026-05-29 11:02:14 +0300

    Update and rename AmmoPickup.cs to AmmoData.cs

---
+++ Scripts/AmmoData.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Класс для хранения данных о боеприпасах.
+/// Используется в системе инвентаря и оружия.
+/// </summary>
+[Serializable]
+public class AmmoData
+{
+    // Тип патронов (например, "9mm", "12gauge", "5.56")
+    public string ammoType;
+
+    // Текущее количество патронов
+    public int currentAmmo;
+
+    // Максимальное количество патронов, которое можно носить
+    public int maxAmmo;
+
+    public AmmoData(string type, int current, int max)
+    {
+        this.ammoType = type;
+        this.currentAmmo = current;
+        this.maxAmmo = max;
+    }
+
+    // Пустой конструктор необходим для корректной сериализации/десериализации JSON
+    public AmmoData()
+    {
+        this.ammoType = "";
+        this.currentAmmo = 0;
+        this.maxAmmo = 0;
+    }
+
+    /// <summary>
+    /// Добавить патроны. Возвращает true, если патроны были добавлены.
+    /// </summary>
+    public bool AddAmmo(int amount)
+    {
+        if (currentAmmo >= maxAmmo)
+            return false;
+
+        currentAmmo += amount;
+        if (currentAmmo > maxAmmo)
+            currentAmmo = maxAmmo;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Потратить патроны. Возвращает true, если хватило патронов.
+    /// </summary>
+    public bool ConsumeAmmo(int amount)
+    {
+        if (currentAmmo >= amount)
+        {
+            currentAmmo -= amount;
+            return true;
+        }
+        return false;
+    }
+}
